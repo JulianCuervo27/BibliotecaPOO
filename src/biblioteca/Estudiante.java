2bf3bdc9d2e168e9c3116f6a0c5386795b907360
JulianCuervo27 <ojcuervoh@academia.usbbog.edu.co>
2026-03-24 14:51:28 +0000

    Subiendo clases estudiante, prestamo

--- src/biblioteca/Estudiante.java
+++ src/biblioteca/Estudiante.java
@@ -1,5 +1,35 @@
 package biblioteca;
+import java.util.Date;
 
-public class Estudiante {
+public class Estudiante extends Persona{
+    private String codigoEstudiante;
+    private String carrera;
     
+    public Estudiante (String id, String nombre, String correo, String codigoEstudiante, String carrera) {
+        super (id, nombre, correo);
+        this.codigoEstudiante = codigoEstudiante;
+        this.carrera = carrera;
+    }
+    
+    public String getCodigoEstudiante() {
+        return codigoEstudiante;
+    }
+    
+    public void setCodigoEstudiante(String codigoEstudiante){
+        this.codigoEstudiante = codigoEstudiante;
+    }
+    
+    public String getCarrera(){
+        return carrera;
+    }
+    
+    public void setCarrera(String carrera){
+        this.carrera = carrera;
+    }
+    
+    public Prestamo solicitarPrestamo(Libro libro, Date fechaPrestamo, Date fechaDevolucion){
+        String idPrestamo = "PREST-" + this.codigoEstudiante + "-" + libro.getCodigoLibro();
+        Prestamo prestamo1 = new Prestamo (idPrestamo, this, libro, fechaPrestamo, fechaDevolucion);
+        return prestamo1;
+    }
 }
--- src/biblioteca/Prestamo.java
+++ src/biblioteca/Prestamo.java
@@ -1,5 +1,55 @@
 package biblioteca;
+import java.util.Date;
 
 public class Prestamo {
+    private String idPrestamo;
+    private Estudiante estudiante;
+    private Libro libro;
+    private Date fechaPrestamo;
+    private Date fechaDevolucion;
     
+    public Prestamo (String idPrestamo, Estudiante estudiante, Libro libro, Date fechaPrestamo, Date fechaDevolucion){
+        this.idPrestamo = idPrestamo;
+        this.estudiante = estudiante;
+        this.libro = libro;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+    
+    public String getIdPrestamo(){
+        return idPrestamo;
+    }
+    
+    public Estudiante getEstudiante(){
+        return estudiante;
+    }
+    
+    public Libro getLibro(){
+        return libro;
+    }
+    
+    public Date getFechaPrestamo(){
+        return fechaPrestamo;
+    }
+    
+    public void setFechaPrestamo(Date fechaPrestamo){
+        this.fechaPrestamo = fechaPrestamo;
+    }
+    
+    public Date getFechaDevolucion(){
+        return fechaDevolucion;
+    }
+    
+    public void setFechaDevolucion(Date fechaDevolucion){
+        this.fechaDevolucion = fechaDevolucion;
+    }
+    
+    public void mostrarInformacionPrestamo(){
+        System.out.println("---- Detalle Prestamo ----");
+        System.out.println("- ID Prestamo: " + idPrestamo);
+        System.out.println("- Estudiante: " + estudiante.getNombre());
+        System.out.println("- Libro: " + libro.getTitulo());
+        System.out.println("- Fecha prestamo: " + fechaPrestamo);
+        System.out.println("- Fecha devolucion: " + fechaDevolucion);
+    }
 }
