14e4b7bfb9af054e8c47eac76684cdffe08f0663
DavidSantiagoCastiblanco <dscastiblancoc@academia.usbbog.edu.co>
2026-03-24 14:46:37 +0000

    persona

--- src/biblioteca/Persona.java
+++ src/biblioteca/Persona.java
@@ -1,5 +1,25 @@
 package biblioteca;
 
 public class Persona {
-    
+    private String id;
+    private String nombre;
+    private String correo;
+
+public persona (String id, String nombre, String correo){
+    this.id = id;
+    this.nombre = nombre;
+    this.correo = correo;
+}
+public String getId () {return id;}
+public void setId (String id) {this.id = id;}
+
+public String getNombre () {return nombre;}
+public void setNombre (String nombre) {this.nombre = nombre;}
+
+public String getCorreo () {return correo;}
+public void setCorreo (String correo) {this.correo = correo;}
+
+public String getDatos() {
+    return nombre + "-" + id;
+}
 }
